6cda10e27d30376d7551c25bd1c469dc4a35fb7f
Ori Grata <42106367+origrata@users.noreply.github.com>
2024-03-01 00:49:28 +0700

    Update whatsapp.php
    Modification to support with wuzapi WA APi

--- whatsapp.php
+++ whatsapp.php
@@ -5,16 +5,14 @@
  *
  * @package    WhatsApp
  * @author     Indra Hartawan <emailme@indrahartawan.com>
+ * @modify     origrata       <origrata@ioscloud.co.id>
  * @copyright  Copyright (c) Indra Hartawan
  * @license    MIT License
  * @version    $Id$
- * @link       https://github.com/indrahartawan/whmcs-hook-whatsapp
+ * @link       https://github.com/origrata/whmcs-hook-whatsapp
  */
-
-
 if (!defined("WHMCS"))
-    die("This file cannot be accessed directly");
-
+   die("This file cannot be accessed directly");
 
 function whatsapp_log($log_message)
 {
@@ -49,9 +47,7 @@ function get_client_details($clientid)
     xml_parse_into_struct($parser, $results, $values, $tags);
     xml_parser_free($parser);
     $client = $results;
-    $client["key"] = $configvars["whatsapp_api_key"];
-    $client["userkey"] = $configvars["whatsapp_api_userkey"];
-    $client["passkey"] = $configvars["whatsapp_api_passkey"];
+    $client["token"] = $configvars["whatsapp_api_token"];
     $client["api_url"] = $configvars["whatsapp_api_url"];
     $client["debug"] = $configvars["debug"];
     if ($client["debug"]) {
@@ -60,57 +56,54 @@ function get_client_details($clientid)
     return $client;
 }
 
-function sent_whatsapp($phone_no,$passkey,$message,$api_url,$userkey,$debug) {
-        $domain = gethostname();
-        $data = array(
-        'userkey' => $userkey,
-        'passkey' => $passkey,
-        'nohp' => $phone_no,
-        'pesan' => $message);
-        $data_string = json_encode($data);
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $api_url);
-curl_setopt($ch, CURLOPT_HEADER, 0);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-curl_setopt($ch, CURLOPT_TIMEOUT,30);
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, array(
-        'userkey' => $userkey,
-        'passkey' => $passkey,
-        'nohp' => $phone_no,
-        'pesan' => $message));
-        $result = json_decode(curl_exec($ch), true);
-        $err = json_decode(curl_error($ch), true);
-        curl_close ($ch);
+function send_whatsapp($phone_no,$token,$message,$api_url,$debug){
+    $domain = gethostname();
+    $data = array(
+        'Phone' => $phone_no,
+        'Body' =>  $message);
+    $bodys = json_encode($data);
+    $curl = curl_init();
+    curl_setopt_array($curl, array(
+            CURLOPT_URL => $api_url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => $bodys,
+            CURLOPT_HTTPHEADER => array(
+                "Content-Type: application/json",
+                "Accept:  application/json",
+                "token:".$token
+            ),
+        ));
+
+        $response = curl_exec($curl);
+        $err = json_decode(curl_error($curl), true);
+        curl_close ($curl);
         if ($err) {
            whatsapp_log("cURL Error : " . $err);
         }
-        return $result;
+       // echo "RESPONSE SEND WA : ".$response."<br/>";
+        return $response;
 }
 
 function whatsapp_new_order($vars) {
    if (isset($_SESSION['uid']) && intval($_SESSION['uid'])!==0){
-   // Checking User Logged In
       $clientdetails = get_client_details($_SESSION['uid']);
-      
       if ($clientdetails['result'] == "success" ) {
-      
       date_default_timezone_set("Asia/Jakarta");
       $today = date("M d, Y H:i:s");
       $firstname = $clientdetails['firstname'];
       $yr_phone_no = $clientdetails['phonenumber'];
-      // replace error phone number format
-      //$yr_phone_no = ereg_replace ("^8","08", $yr_phone_no); #deprecated on PHP 7.x
       $yr_phone_no = preg_replace ("/^8/","08", $yr_phone_no);
-      //$phone_no = "08XXXXXXXXXX";
       $phone_no = $yr_phone_no;
       $orderid = $vars['OrderID'];
       $ordernumber = $vars['OrderNumber'];
       $invoiceid = $vars['InvoiceID'];
       $amount = "Rp. ". number_format($vars['TotalDue'],2);
-
       //define payment method based on the database to make it friendly. Add yourself based on the value in the 'PaymentMethod'
       switch($vars['PaymentMethod']) {
           case "va_mandiri":
@@ -120,45 +113,44 @@ function whatsapp_new_order($vars) {
                $paymentmethod = $vars['PaymentMethod'];
                break;
       }
+       $message = "Hi ". $firstname . ",
 
-       $message = "Hi ". $firstname . ", 
-
-Ini adalah pesan otomatis. Terima kasih telah melakukan pemesanan di Exabytes Indonesia pada ". $today .".
+Ini adalah pesan otomatis. Terima kasih telah melakukan pemesanan di Ioscloud Indonesia pada ". $today .".
 
 *Berikut detail pemesanan Anda :*
 Nomor pemesanan : ". $ordernumber ."
 Metode pembayaran : ". $paymentmethod ."
 Total Pembelian : ". $amount ."
-Lihat Invoice : https://billing.exabytes.co.id/viewinvoice.php?id=". $invoiceid ."
+Lihat Invoice : https://my.ioscloud.co.id/viewinvoice.php?id=". $invoiceid ."
 (harap login ke client area untuk melihat invoice dan melakukan pembayaran)
 
-Pertanyaan soal pembayaran di Exabytes:
-https://www.exabytes.co.id/pembayaran
+Pertanyaan soal pembayaran di ioscloud:
+https://www.ioscloud.co.id/pembayaran
 
-Promo terbaik Exabytes:
-https://www.exabytes.co.id/promo
+Promo terbaik ioscloud:
+https://www.ioscloud.co.id/promo
 
-Jika mengalami kendala bisa menghubungi kami via chat di nomor official berikut 082215468046.
+Jika mengalami kendala bisa menghubungi kami via chat di nomor official berikut:
+origrata    ---> 081268895343
+henri ilham --->
+syaflan     --->
 
-_Silahkan abaikan pesan ini jika pembayaran Anda telah diselesaikan._ 
+_Silahkan abaikan pesan ini jika pembayaran Anda telah diselesaikan._
 Terima kasih.";
 
         $phone_no = preg_replace( "/(\n)/", ",", $phone_no );
         $phone_no = preg_replace( "/(\r)/", "", $phone_no );
         $api_url = $clientdetails['api_url'];
-        $userkey = $clientdetails['userkey'];
-        $passkey = $clientdetails['passkey'];
+        $token = $clientdetails['token'];
         if ($clientdetails['debug']){
-           $msg = "phone_no -> " . $phone_no . " | userkey -> " . $userkey . " | passkey -> " . $passkey . " | api_url -> " . $api_url;
+           $msg = "phone_no -> " . $phone_no . " | token -> " . $token . "  | api_url -> " . $api_url;
            whatsapp_log($msg);
         }
-        $result = sent_whatsapp($phone_no,$passkey,$message,$api_url,$userkey,$clientdetails['debug']);
+        $result = send_whatsapp($phone_no,$token,$message,$api_url,$clientdetails['debug']);
         if ($clientdetails['debug']){ whatsapp_log("Message successfully sent to " . $phone_no . ""); }
      }
   }
 
 }
-
 add_hook("AfterShoppingCartCheckout",999,"whatsapp_new_order");
-
 ?>
